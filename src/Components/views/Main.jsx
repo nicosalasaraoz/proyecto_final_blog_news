fa419b0d31b4c93afac435e412973e15c3bb8d38
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2023-05-19 19:16:32 -0300

    trayendo la base de datos a app y a main

--- src/Components/views/Main.jsx
+++ src/Components/views/Main.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Container} from 'react-bootstrap';
 import Header from "./Header";  
 import Articles from "./news/Articles";
+import { useState, useEffect } from "react";
+import { consultarAPI } from "../helpers/queries";
 
+const Main = ({setSearchState, searchState}) => {
+  const [News, setNews] = useState([]);
 
-
-const Main = ({ News, setSearchState, searchState}) => {
-  
+    useEffect(() => {
+        consultarAPI().then((respuesta) => {
+            //const prueba =consultarAPI(); para obtener el return
+            setNews(respuesta);
+        });
+    }, []);
   return (
     <>
       <Header setSearchState={setSearchState} searchState={searchState}/>
@@ -14,9 +21,7 @@ const Main = ({ News, setSearchState, searchState}) => {
       <h1 className="display-3 py-3 text-center">Artículos destacados</h1>
       <hr />
       <div className="cards">
-        {
-            News.map((Art)=> <Articles key={Art.id} Art={Art}></Articles>)            
-        }
+        {News.map((Art)=> <Articles key={Art._id} Art={Art}></Articles>)}
       </div>
     </Container>
     </>
--- src/Components/views/news/Articles.jsx
+++ src/Components/views/news/Articles.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import { BiLike } from "react-icons/bi";
 
 const Articles = ({Art}) => {
-  
   const { id, title, url } = { ...Art };
+
   return (
     <>
     <div className="Card">
