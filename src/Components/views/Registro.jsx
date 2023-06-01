212ae33c0867cf014d175ec11a06e017482238b4
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2023-06-01 19:12:18 -0300

    maquetando register y agregando campos

--- src/Components/views/Registro.jsx
+++ src/Components/views/Registro.jsx
@@ -39,7 +39,7 @@ const Registro = ({ setUsuarioLogueado }) => {
             </section>
             <Form onSubmit={handleSubmit(onSubmitRegistro)} noValidate className="border rounded p-3 shadow">
                 <Row>
-                    <Col sm={12} md={6}>
+                    <Col>
                         <Form.Group className="mb-3" controlId="formRegisterName">
                             <Form.Label>Nombre</Form.Label>
                             <Form.Control
@@ -59,31 +59,7 @@ const Registro = ({ setUsuarioLogueado }) => {
                             />
                             <Form.Text className="text-danger">{errors.nombre?.message}</Form.Text>
                         </Form.Group>
-                    </Col>
-                    <Col sm={12} md={6}>
-                        <Form.Group className="mb-3" controlId="formRegisterLastName">
-                            <Form.Label>Apellido</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Ingrese su apellido"
-                                {...register("apellido", {
-                                    required: "Ingrese su apellido",
-                                    minLength: {
-                                        value: 2,
-                                        message: "La cantidad mínima de caracteres debe ser 2",
-                                    },
-                                    maxLength: {
-                                        value: 50,
-                                        message: "La cantidad máxima de caracteres debe ser 50",
-                                    },
-                                })}
-                            />
-                            <Form.Text className="text-danger">{errors.apellido?.message}</Form.Text>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={12} md={6}>
+                   
                         <Form.Group className="mb-3" controlId="formRegisterEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
@@ -99,13 +75,29 @@ const Registro = ({ setUsuarioLogueado }) => {
                             />
                             <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
                         </Form.Group>
-                    </Col>
-                    <Col sm={12} md={6}>
+                
                         <Form.Group className="mb-3" controlId="formRegisterPassword">
                             <Form.Label>Contraseña</Form.Label>
                             <Form.Control
                                 type="password"
                                 placeholder="Password"
+                                {...register("password", {
+                                    required: "Ingrese su contraseña",
+                                    pattern: {
+                                        value: /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                                        message:
+                                            "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.",
+                                    },
+                                })}
+                            />
+                            <Form.Text className="text-danger">{errors.password?.message}</Form.Text>
+                        </Form.Group>
+                   
+                        <Form.Group className="mb-3" controlId="formRegisterPassword">
+                            <Form.Label>Repita contraseña</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="RepeatPassword"
                                 {...register("password", {
                                     required: "Ingrese su contraseña",
                                     pattern: {
