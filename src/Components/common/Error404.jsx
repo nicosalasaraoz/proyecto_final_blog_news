f0ca0a82d61f3d7c121a8ceeae50c61b247e2766
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2023-07-11 11:06:37 -0300

    cambiando el boton de error 404

--- src/Components/common/Error404.jsx
+++ src/Components/common/Error404.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <Container className="image-e btn">
+    <Container className="image-e d-flex justify-content-center">
       <Link to="/" className="text-white" style={{ textDecoration: "none" }}>
         <Button className="bt-volver">
           Back to Home
