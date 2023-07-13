d51fd57385a396eb9e41e148d7030b4db8fcd991
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2023-07-13 15:42:17 -0300

    ordenando las cards del search

--- src/Components/views/Search.jsx
+++ src/Components/views/Search.jsx
@@ -5,20 +5,19 @@ import { Button, Card, Container } from "react-bootstrap";
 const Search = ({nuevo, searchState}) => {
   
   const [articles, setArticles] = useState([]);
-  console.log(nuevo)
-  const prueba = () =>{
 
+  useEffect(() => {prueba()}, [searchState]);
+
+  const prueba = () =>{
+    
     let res = nuevo.filter((dato) => dato.title.toLowerCase().includes(searchState.toLowerCase()));
     setArticles(res);
   } 
-  useEffect(() => {
-    prueba();
-   }, [searchState]);
-
+  
   return (
     <>
       <Container>
-        <div className="Buscador">
+        <div className="Buscador cards">
           {articles[0]?(articles.map((article) => (
             <div key={article._id} className="Card">
               <Card.Img variant="top" src={article.url} />
